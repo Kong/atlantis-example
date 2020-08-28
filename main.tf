e2dd040ff7984a1d3f28f9157541553dc53df615
Philippe M. Chiasson <gozer@ectoplasm.org>
2020-08-27 21:32:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ provider "random" {
   version = "~> 2.2"
 }
 
-resource "random_password" "password" {
-  length = 16
-  special = false
-}
-
 resource "aws_s3_bucket" "test" {
-  bucket_prefix = "${random_password.password.result}-"
+  bucket_prefix = "gozer-123-trestXXX-"
 }
